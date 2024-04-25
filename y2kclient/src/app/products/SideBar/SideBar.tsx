ae0bffd4e96ce9ee2f14193ef39e03e5017980a2
Marcos Ramirez <gellollo@outlook.com>
2024-04-25 12:50:44 -0300

    Como chota le pongo estilo al select la concha

--- y2kclient/src/app/products/SideBar/SideBar.tsx
+++ y2kclient/src/app/products/SideBar/SideBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/hooks";
-import { sortByCategory } from "@/lib/slices/productsReducer";
+import { sortByCategory, sortBySize } from "@/lib/slices/productsReducer";
 import { getAllProducts } from "@/lib/actions/getAllProducts";
 import style from "./SideBar.module.css";
 import {
@@ -35,6 +35,9 @@ export default function SideBar() {
   const handleFilterCategory = (category: string) => {
     dispatch(sortByCategory(category));
   };
+  const handleSizeCategory = (size: string) =>{
+    dispatch(sortBySize(size))
+  }
 
   const getUniqueCategories = (items: ProductCategory[]): string[] => {
     const categoriesSet = new Set<string>();
@@ -48,11 +51,14 @@ export default function SideBar() {
     const sizesSet = new Set<string>();
     items.forEach(product => {
       product.sizes.forEach(category => {
-        sizesSet.add(sizes.sizes);
+        sizesSet.add(category);
       });
     });
-    return Array.from(categoriesSet);
+    return Array.from(sizesSet);
   }
+
+  const allSizes = getUniqueSizes(stateProducts)
+ 
 
   const allCategories = getUniqueCategories(stateProducts)
 
@@ -63,14 +69,14 @@ export default function SideBar() {
   return (
     <div>
       <div
-        className={`flex flex-col bg-gray-800 top-[8rem] transition-all duration-300 z-[100] left-0 fixed self-start w-[15%] h-[100vh] ${
+        className={`flex flex-col bg-gray-800 gap-5 p-2 top-[8rem] transition-all duration-300 z-[100] left-0 fixed self-start w-[15%] h-[100vh] ${
           statusBar ? "translate-x-0" : "translate-x-[-50vw]"
         } `}
       >
         <button className="text-3xl text-gray-100 p-5" onClick={handleBar}>
           <IoIosArrowDropleftCircle />
         </button>
-
+<p>Select category</p>
         <select onChange={(e) => handleFilterCategory(e.target.value)}>
           <option value="all">All Categories</option>
           {allCategories.map((category, index) => (
@@ -80,21 +86,16 @@ export default function SideBar() {
           ))}
         </select>
 
-        <select>
-          <option value="">All Categories</option>
-            <option> 
-              S
+<p>Select Size</p>
+        <select className="relative caret-blue-200 p-2 bg-blue-300 rounded-[1.25rem] font-tiltneon text-xl text-blue-950 font-normal transition-all duration-300 after:rounded hover:scale-105 hover:ring-2 hover:ring-blue-200" onChange={(e) => handleSizeCategory(e.target.value)}> 
+          <option className="bg-blue-200 p-2 rounded-[1rem]" value="all">All Sizes</option>
+          {allSizes.map((size, index) => (
+            <option className="bg-blue-200 p-2 rounded-[1rem]" key={index} value={size}>
+              {size}
             </option>
-            <option> 
-              M
-            </option>
-            <option> 
-              L
-            </option>
-            <option> 
-              XL
-            </option>
+          ))}
 
+         
         </select>
       </div>
       {!statusBar ? (
