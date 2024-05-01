5c389889c53d5d0d0e98e089e3133a1d4835d4be
Marcos Ramirez <gellollo@outlook.com>
2024-05-01 14:30:11 -0300

    cambios a la navbar

--- y2kclient/src/app/[locale]/Components/NavBar/Buttons/Buttons.tsx
+++ y2kclient/src/app/[locale]/Components/NavBar/Buttons/Buttons.tsx
@@ -1,53 +1,54 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
-import style from '../NavBar.module.css'
+import style from "../NavBar.module.css";
 import { FaBagShopping } from "react-icons/fa6";
 import { FaIdBadge } from "react-icons/fa";
 
-const ButtonsNavBar = () =>{
-    const [stateButton, setStateButton] = useState<string>()
+const ButtonsNavBar = () => {
+  const [stateButton, setStateButton] = useState<string>();
 
-
-    return(
-        <div className="gap-3 lg:gap-[5rem] flex flex-row">
-        <Link href={{ pathname: "/cart" }}>
-          <button onMouseEnter={() => setStateButton('cart')}
-        onMouseLeave={() => setStateButton('none')} className="flex flex-col font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
-            <FaCartShopping />
-          </button>
-          {stateButton === 'cart'? 
-          <span className="absolute justify-self-center bg-white text-black p-2 rounded-md shadow">
-          Cart
-        </span> : null
-          }
-        </Link>
-        <Link  href={{ pathname: "/products" }}>
-          <button onMouseEnter={() => setStateButton('product')}
-        onMouseLeave={() => setStateButton('none')} className="flex flex-col font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
-            <FaBagShopping />
-          </button>
-          {stateButton === 'product'? 
-          <span className={style.scaleBottom}>
-        Buy 
-        </span> : null
-          }
-        </Link>
-        <Link href={{ pathname: "/" }}>
-        <button onMouseEnter={() => setStateButton('account')}
-        onMouseLeave={() => setStateButton('none')} className=" flex px-4 flex-col font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
+  return (
+    <div className="gap-3 lg:gap-[5rem] flex flex-row">
+      <Link className="flex flex-col items-center justify-center"
+        onMouseLeave={() => setStateButton("none")}
+        onMouseEnter={() => setStateButton("cart")}
+        href={{ pathname: "/cart" }}
+      >
+        <button className="flex flex-col items-center justify-center font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
+          <FaCartShopping />
+        </button>
+        {stateButton === "cart" ? (
+          <span className={style.scaleBottom}>Cart</span>
+        ) : null}
+      </Link>
+      <Link className="flex flex-col items-center justify-center"
+        onMouseEnter={() => setStateButton("product")}
+        onMouseLeave={() => setStateButton("none")}
+        href={{ pathname: "/products" }}
+      >
+        <button className="flex flex-col font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
+          <FaBagShopping />
+        </button>
+        {stateButton === "product" ? (
+          <span className={style.scaleBottom}>Buy</span>
+        ) : null}
+      </Link>
+      <Link className="flex flex-col items-center justify-center"
+        onMouseEnter={() => setStateButton("account")}
+        onMouseLeave={() => setStateButton("none")}
+        href={{ pathname: "/" }}
+      >
+        <button className=" flex flex-col font-titilium text-4xl lg:text-5xl text-pink-50 font-normal">
           <FaIdBadge />
         </button>
-        {stateButton === 'account'? 
-          <span className="absolute justify-self-center bg-white text-black p-2 rounded-md shadow">
-        Account
-        </span> : null
-          }
-        </Link>
-       
-      </div>
-    )
-}
+        {stateButton === "account" ? (
+          <span className={style.scaleBottom}>Account</span>
+        ) : null}
+      </Link>
+    </div>
+  );
+};
 
-export default ButtonsNavBar
+export default ButtonsNavBar;
--- y2kclient/src/app/[locale]/Components/NavBar/NavBar.tsx
+++ y2kclient/src/app/[locale]/Components/NavBar/NavBar.tsx
@@ -8,7 +8,14 @@ export default function Navbar() {
   return (
     <div className="flex flex-row w-full p-4 lg:p-10 h-32 bg-gray-950 fixed justify-between z-[100]">
       <Link href={{ pathname: "/" }}>
-        <button className={style.Title}>Y2K Shop</button>
+        <div className="flex flex-row">
+          <p className="font-titilium text-5xl font-bold text-pink-300 [text-shadow:_7px_0px_6px_#80dde0;] ">Y</p>
+          <p className="font-titilium text-5xl font-bold text-blue-300 [text-shadow:_7px_0px_6px_#ffa1d3;]" >2</p>
+          <p className="font-titilium text-5xl font-bold text-pink-300 [text-shadow:_7px_0px_6px_#80dde0;]">K</p>
+          <p className="font-titilium text-5xl font-bold text-blue-300 [text-shadow:_7px_0px_6px_#ffa1d3;]">Fashion</p>
+
+
+        </div>
       </Link>
       <ButtonsNavBar/>
     </div>
